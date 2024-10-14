0ada0076f37a5a2cda2c6c32c99d7fce749f3d8a
amritsaini2024 <amrit.saini@designersx.com>
2024-10-14 16:47:35 +0530

    login changes

--- src/componets/RouteGuard/index.js
+++ src/componets/RouteGuard/index.js
@@ -61,6 +61,12 @@ function RouteGuard({ children }) {
                 );
                 window.location.href = "/account";
               }
+              if (split.length == 3) {
+                let cartId = split[2] ?? null;
+                window.location.href = `https://www.brunomd.eu/cart/c/${cartId}`;
+              } else {
+                window.location.href = window.location.origin + "/account";
+              }
             })
             .catch((err) => {
               console.log({ err });
@@ -162,7 +168,7 @@ function RouteGuard({ children }) {
       "/disclaimer",
       "/site-map",
       "/tendo",
-      "/science"
+      "/science",
       //"/tendo-V2",
     ];
     if (!publicPaths.length) {
