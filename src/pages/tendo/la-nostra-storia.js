7b5ebf99eb374c22476ae90ec2ef8131d3a93be0
NirajDX <niraj.t@designersx.com>
2025-02-28 10:57:04 +0530

    [SS] update  tendo  footer changes (28-2-25)(SS.1.1)

--- src/pages/tendo/la-nostra-storia.js
+++ src/pages/tendo/la-nostra-storia.js
@@ -11,14 +11,28 @@ import BrunoFamily from '@/utilities/BrunoFamily/BrunoFamily';
 import OurMap from '@/utilities/OurMap/OurMap';
 import OurStory from '@/utilities/OurStory/OurStory';
 import MasterHeadImg2 from '@/utilities/MasterHeadImg2';
-
+import Footer from "@/utilities/Footer/";
+import data1 from "../../../json/layout.json";
+import AnnouncementBar2 from "@/utilities/announcementBar2";
 export default function Storia({version,script}) {
     const {masterHead,timelineDesc, section,timeline,newsletter} = storia[version] || {};
     return(
         <section>
+           {data1["EU"].announcementBar && (
+        <AnnouncementBar2
+          announcement={data1["EU"].announcementBar.title2}
+          theme={{
+            textColor: data1["EU"].announcementBar.textColor,
+            backgroundColor: document.referrer.includes("tendo")
+              ? "rgb(0, 51, 161)"
+              : data1["EU"].announcementBar.backgroundColor,
+          }}
+        />
+      )}
         <PageHead content={SEO[version].storia.SEO} />
         {/* <MasterHeadImg  data={masterHead}/> */}
         <MasterHeadImg2 data={masterHead}/>
+
      
         <div className={styles.container}>
                 <div className={styles.desc}>
@@ -31,7 +45,9 @@ export default function Storia({version,script}) {
         <OurMap/>
         <ImgText data={section}/>
         <OurStory/>
+         <Footer data={data1["EU"].footer2} />
         </section>
     )
 
 }
+Storia.hideLayout = true;
---
+++ src/utilities/announcementBar2/index.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import styles from "./styles.module.css";
+import RightStickyScroll from "@/utilities/RightStickyScroll/RightStickyScroll";
+import { CartIcon, SearchIcon } from "../SvgIcons";
+import { CartItemNumber } from "@/data/lib";
+import { FaCartShopping } from "react-icons/fa6";
+import Link from "next/link";
+
+
+const AnnouncementBar2 = ({ announcement, theme, ModalHandler, position }) => {
+  const [itemNumber, setItemNumber] = useState(0);
+  useEffect(() => {
+    CartItemNumber()
+      .then((response) => {
+        setItemNumber(
+          response?.data?.cart?.lines?.edges?.length > 0
+            ? response?.data?.cart?.lines?.edges?.length
+            : 0
+        );
+      })
+      .catch((err) => {
+        console.log({ err });
+      });
+  }, []);
+
+  React.useEffect(() => {
+    window.onscroll = function () {
+      scrollFunction();
+    };
+    function scrollFunction() {
+      if (
+        document.body.scrollTop > 50 ||
+        document.documentElement.scrollTop > 50
+      ) {
+        if (document.getElementById("fixedAnnouncementBar"))
+          document.getElementById("fixedAnnouncementBar").style.position =
+            "fixed";
+        if (document.getElementById("fixedAnnouncementBar")) {
+          document.getElementById("fixedAnnouncementBar").style.top = "0";
+        }
+      } else {
+        if (document.getElementById("fixedAnnouncementBar"))
+          document.getElementById("fixedAnnouncementBar").style.position =
+            "fixed";
+      }
+    }
+  }, []);
+  if (!ModalHandler) {
+    return (
+      <section
+        className={styles.announcementBar}
+        id="fixedAnnouncementBar"
+        style={{
+          color: theme?.textColor,
+          backgroundColor: theme?.backgroundColor,
+        }}
+      >
+
+<div style={{ width: "80px", cursor: "pointer" }}>
+  <Link href="/tendo" style={{ textDecoration: "none", color: "inherit" }}>
+    <span style={{ fontSize: "18px" }}> GO Back</span>
+  </Link>
+</div>
+        {/* <div dangerouslySetInnerHTML={{__html:announcement}}/> */}
+        <div
+          style={{ width: "100%" ,textAlign:"center"}}
+          // behavior="scroll"
+          // scrollamount="6"
+          
+        >
+          <span><b>TENDOACTIVE PLUS-</b>OFFERTA ESCLUSIVA SCONTO DEL 20% SU OGNI ACQUISTO PERIODICO</span>
+        </div>
+        <div className={styles.carticon} onClick={() => (window.location.href = "/carrello")}>
+          <CartIcon
+            number={itemNumber === 0 ? "" : itemNumber}
+            styles={{ paddingTop: "2px", }}
+          />
+        </div>
+      </section>
+    );
+  } else {
+    return (
+      <section
+        className={
+          position ? styles.fixedAnnouncementBar : styles.announcementBar
+        }
+        style={{
+          color: theme?.textColor,
+          backgroundColor: theme?.backgroundColor,
+        }}
+      >
+        <div
+          style={{ margin: "auto" }}
+          dangerouslySetInnerHTML={{ __html: announcement }}
+        />
+      </section>
+    );
+  }
+};
+
+export default AnnouncementBar2;
